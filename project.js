3b6a415a040901975fdcc5c79452fc098c51c116
Kevin Li <kevinsl192k@gmail.com>
2023-05-22 15:49:48 -0700

    renamed class Assignemnt3 to Project

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
 
-export class Assignment3 extends Scene {
+export class Project extends Scene {
     constructor() {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
