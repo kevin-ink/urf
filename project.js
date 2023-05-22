a194679b52d39e954f71eb3f3fd3f18ff36d654f
Kevin Li <kevinsl192k@gmail.com>
2023-05-22 15:54:39 -0700

    brought back common.js LMAO

--- project.js
+++ project.js
@@ -1,3 +1,5 @@
+import { defs, tiny } from "./examples/common.js";
+
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
