c3e27202c0048ec90f0474911b3f138e1d9623d8
Kevin Li <kevinsl192k@gmail.com>
2023-05-23 01:17:55 -0700

    added some animations to the main menu

--- ui.js
+++ ui.js
@@ -6,5 +6,22 @@ id = setInterval(() => {
     h1.textContent = h1.textContent == "ULTRA RAPID FIRE_" ? "ULTRA RAPID FIRE " : "ULTRA RAPID FIRE_";
 }, 500);
 
-const opt_popup = document.createElement("div");
-opt_popup.setAttribute("id", "options")
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+    if (btn.id !== "start-btn") {
+        btn.addEventListener("click", () => {
+            btn.classList.add("popup");
+            btn.style.transform = `translateY(-30vh)`;
+            btn.style.transform += btn.id === "opt-btn" ? `translateX(46vh)` : `translateX(-50vh)`;
+            btns.forEach((other) => {
+                if (other !== btn) {
+                    other.disabled = true;
+                    other.classList.add("animate__animated", "animate__fadeOutDown");
+                    other.addEventListener("animationend", () => {
+                        htpBtn.style.visibility = "hidden";
+                    })
+                }
+            })
+        })
+    }
+})
