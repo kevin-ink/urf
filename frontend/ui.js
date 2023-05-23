dbc36012ca806ca5bcbb10f1ea8dec0dc5012de7
Kevin <kevinsl192k@gmail.com>
2023-05-23 16:14:26 -0700

    adjusted once again

--- frontend/ui.js
+++ frontend/ui.js
@@ -24,7 +24,7 @@ function expand(e) {
             other.classList.add("move");
             other.style.transform = `translateY(-30vh)`;
             other.style.transform +=
-                other.id === "opt-btn" ? `translateX(32vw)` : `translateX(-32vw)`;
+                other.id === "opt-btn" ? `translateX(31vw)` : `translateX(-31vw)`;
         }
         else 
         {
